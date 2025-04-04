7dd6114c4882363f75efd23fa4f34700bfd65c42
Charanteja <37008444+charan379@users.noreply.github.com>
2025-04-04 16:25:04 +0530

    Create leftAngleTriangle.js

---
+++ arrays/leftAngleTriangle.js
@@ -0,0 +1,19 @@
+const args = process.argv.slice(2);
+
+const rows = parseInt(args[0]) ?? 5;
+
+for (let i = 0; i < rows; i++) {
+    // console.log("  ".repeat(rows - i - 1) + " *".repeat(i + 1));
+    // 
+    let space = "  ";
+    for (let j = 0; j < rows - i - 1; j++) {
+        space += "  ";
+    }
+
+    let star = " *";
+    for (let y = 0; y < i; y++) {
+        star += " *";
+    };
+
+    console.log(space + star);
+};
